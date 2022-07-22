4e8c0ad0e8e1e7fd175a28410eca85c24b85243f
Leigh Ryffel <leightryffel@gmail.com>
2022-07-22 15:32:03 -0400

    created activity filter button and did styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function NavBar({ setUser }) {
   function handleLogoutClick() {
@@ -12,17 +13,17 @@ function NavBar({ setUser }) {
     });
   }
   return (
-    <div>
-      <Link to="/">
-        <h1>NPS Digital Passport</h1>
-      </Link>
-      <Link to="/passport">
-        <button>View Passport</button>
-      </Link>
-      <Link to="/profile">
-        <button>Update Profile</button>
-      </Link>
-      <button onClick={handleLogoutClick}>Log Out</button>
+    <div className="NavBar">
+        <Link to="/">
+          <h1>NPS Digital Passport</h1>
+        </Link>
+        <Link to="/passport">
+          <button>View Passport</button>
+        </Link>
+        <Link to="/profile">
+          <button>Update Profile</button>
+        </Link>
+        <button onClick={handleLogoutClick}>Log Out</button>
     </div>
   );
 }
