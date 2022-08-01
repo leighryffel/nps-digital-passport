9e3bcc9a02b2f0ee042bd4097fad725e0c3a46dd
Leigh Ryffel <leightryffel@gmail.com>
2022-08-01 11:32:18 -0400

    updated navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
+// import Button from "@mui/material/Button";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import CssBaseline from "@mui/material/CssBaseline";
